fda4e156f684bdc486f0109171d1a8bc4510e547
ishwaryachitsen0523 <ishwaryachitsen98@gmail.com>
2019-02-01 12:28:37 +0530

    Create str.py

---
+++ str.py
@@ -0,0 +1,6 @@
+str_input1 = input("Enter First String? ")
+str_input2 = input("Enter Second String? ")
+if str_input1 == str_input2:
+    print ("First and second strings are same!")
+else:
+    print("you entered a different string")
